d7a6c7e9cf8207f725f2ef0733fe4690bd72878d
jjaahhhhh-g <jarellgarcia1819@gmail.com>
2026-01-14 22:48:00 +0800

    APIconfig

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://bdmxzqdmkextlfvszwxd.supabase.co';
-const supabaseAnonKey = 'sb_publishable_jDnsZB90G9AruodQbV7r0A_rYwy_VHc';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY ;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
